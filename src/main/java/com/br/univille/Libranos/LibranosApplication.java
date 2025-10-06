26a34232f57e49db158de05add85ad92ce9a1441
univille <univille@LAB-SIS-NOTE-40.univille.br>
2025-10-06 20:09:46 -0300

    first

---
+++ src/main/java/com/br/univille/Libranos/LibranosApplication.java
@@ -0,0 +1,13 @@
+package com.br.univille.Libranos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibranosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibranosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/univille/Libranos/LibranosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.univille.Libranos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibranosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
